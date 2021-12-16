34ff24c9cd61b0d7e6e86b9ba762b20c5d303375
maksimvelma <maks.velma@yandex.ru>
2021-12-16 15:53:09 +0200

    Create program5.1.cs

---
+++ program5.1.cs
@@ -0,0 +1,66 @@
+using System;
+
+namespace lab5
+{
+    interface IShape
+    {
+        string sizeFigure { get; }
+        string Type();
+        string Square();
+    }
+    interface IColoredShape : IShape
+    {
+        string color { set; }
+        string ShowColor();
+    }
+    class Circle : IShape
+    {
+        protected double r { get; set; }
+        public Circle(double r)
+        {
+            this.r = r;
+        }
+        public string sizeFigure
+        {
+            get
+            {
+                return $"Розмiр фiгури: {r}";
+            }
+        }
+        public string Square()
+        {
+            double s = Math.PI*(r*r);
+            return $"Площа фiгури = {s.ToString("F" + 5)}";
+        }
+        public string Type()
+        {
+            return "Тип: коло";
+        }
+    }
+    class ColoredCircle : Circle, IColoredShape
+    {
+        public string color;
+        public ColoredCircle(double r, string color) : base (r)
+        {
+            this.color = color;
+        }
+
+        string IColoredShape.color { set => Console.ReadLine(); }
+
+        public string ShowColor()
+        {
+            return $"Колiр: {color}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColoredCircle[] rectangle = { new ColoredCircle (2.75,"чорний"), new ColoredCircle (6.25,"бiлий"), new ColoredCircle (7.5,"жовтий") };
+            foreach (ColoredCircle str in rectangle)
+            {
+                Console.WriteLine($"{str.Type()}\n{str.sizeFigure}\n{str.Square()}\n{str.ShowColor()}\n");
+            }
+        }
+    }
+}
